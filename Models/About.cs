96d29b481dcaad64b97c4e1641cf030f403b068a
Peter Spangler <petertspangler@outlook.com>
2023-02-13 14:50:39 -0700

    updated notes page and about page models

---
+++ Models/About.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChoreHub2._0.Models
+{
+    internal class About
+    {
+        public string Title => AppInfo.Name;
+        public string Version => AppInfo.VersionString;
+        public string MoreInforUrl => "https://aka.ms/maui";
+        public string Message => "This app is writting in XAML and C# with .NET MAUI.";
+    }
+}
--- Views/NotePage.xaml.cs
+++ Views/NotePage.xaml.cs
@@ -1,4 +1,4 @@
-namespace ChoreHub2._0;
+namespace ChoreHub2._0.Views;
 
 public partial class NotePage : ContentPage
 {
