6fe3aeb8b3849799d78df0a90cd4eb2789087986
Peter Spangler <petertspangler@outlook.com>
2023-02-24 12:06:20 -0700

    Big issues with compile

--- Views/ChorePage.xaml.cs
+++ Views/ChorePage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace ChoreHub2._0.Views
+﻿namespace ChoreHub2._0.Views
 {
     public partial class ChorePage : ContentPage
     {
