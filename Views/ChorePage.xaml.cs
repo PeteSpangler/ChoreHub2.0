47b0553d069dba9781ad3b1916561e4e04f2bb8b
Peter Spangler <petertspangler@outlook.com>
2023-02-24 11:34:29 -0700

    Added Chore Views/Models/ViewModels

---
+++ Views/ChorePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ChoreHub2._0.Views
+{
+    public partial class ChorePage : ContentPage
+    {
+        public ChorePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
