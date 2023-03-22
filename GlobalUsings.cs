14c0672d3410628b54232b499c0ce57b8ca1ca4f
Peter Spangler <petertspangler@outlook.com>
2023-03-22 13:04:51 -0600

    added global usings, trying to get the icons working

---
+++ GlobalUsings.cs
@@ -0,0 +1,13 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using ChoreHub2._0.Repositories;
+global using ChoreHub2._0.Models;
+global using ChoreHub2._0.ViewModels;
+global using ChoreHub2._0.Views;
+
+
